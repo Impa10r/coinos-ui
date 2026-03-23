88694f8f97677aab8cea3f51070639268aee6ad6
Impa10r <101550606+Impa10r@users.noreply.github.com>
2026-03-23 23:34:31 +0100

    fix allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,30 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 import path from "path";
 
-export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
-  resolve: {
-    alias: {
-      $comp: path.resolve("src/components"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [tailwindcss(), sveltekit()],
+    resolve: {
+      alias: {
+        $comp: path.resolve("src/components"),
+      },
     },
-  },
-  server: {
-    allowedHosts: process.env.PUBLIC_DOMAIN ? [process.env.PUBLIC_DOMAIN] : [],
-    proxy:
-      process.env.NODE_ENV === "development"
-        ? {
-            "/api/": {
-              target: "http://localhost:3119",
-              rewrite: (path) => path.replace(/^\/api\//, ""),
-            },
-          }
-        : undefined,
-  },
+    server: {
+      allowedHosts: env.PUBLIC_DOMAIN ? [env.PUBLIC_DOMAIN] : [],
+      proxy:
+        process.env.NODE_ENV === "development"
+          ? {
+              "/api/": {
+                target: "http://localhost:3119",
+                rewrite: (path) => path.replace(/^\/api\//, ""),
+              },
+            }
+          : undefined,
+    },
+  };
 });

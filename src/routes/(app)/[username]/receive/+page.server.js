91987c7bb6145dd303bfd46fb59fd148691c5c80
Impa10r <101550606+Impa10r@users.noreply.github.com>
2026-03-29 17:08:11 +0200

    Fix: always use clearnet domain in LN addresses

--- src/routes/(app)/[username]/receive/+page.server.js
+++ src/routes/(app)/[username]/receive/+page.server.js
@@ -1,13 +1,14 @@
 import getRates from "$lib/rates";
 import { redirect } from "@sveltejs/kit";
+import { PUBLIC_DOMAIN } from "$env/static/public";
 
-export const load = async ({ cookies, params: { username }, parent, url }) => {
+export const load = async ({ cookies, params: { username }, parent }) => {
   const token = cookies.get("token");
   const rates = await getRates();
   if (!token) redirect(307, `/pay/${username}`);
   const { subject } = await parent();
   return {
     rate: rates[subject.currency],
-    text: `${username}@${url.host}`,
+    text: `${username}@${PUBLIC_DOMAIN}`,
   };
 };

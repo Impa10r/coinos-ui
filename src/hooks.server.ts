9796d79786fe88425ce43c4b4937d553f9ee8774
Impa10r <101550606+Impa10r@users.noreply.github.com>
2026-04-04 20:40:43 +0200

    undo response headers

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -13,13 +13,11 @@ export const handle: Handle = async ({ event, resolve }) => {
 
   const response = await ipStore.run(ip, () => resolve(event));
 
-  if (PUBLIC_DOMAIN_TOR && response.headers.get("content-type")?.startsWith("text/html")) {
-    const headers = new Headers(response.headers);
-    headers.set(
+  if (PUBLIC_DOMAIN_TOR) {
+    response.headers.set(
       "Onion-Location",
       `http://${PUBLIC_DOMAIN_TOR}${event.url.pathname}${event.url.search}`,
     );
-    return new Response(response.body, { status: response.status, statusText: response.statusText, headers });
   }
 
   return response;
